1117f30783a536567d3893233f12087fa530a9ee
Tom Tzook <tomtzook@gmail.com>
2021-01-15 09:52:50 +0200

    segments

--- arch/include/x86/segment.h
+++ arch/include/x86/segment.h
@@ -5,13 +5,70 @@
 
 namespace x86 {
 
+enum selector_table_t {
+    TABLE_GDT = 0,
+    TABLE_LDT = 1
+};
+
+enum data_type_t {
+    READ_ONLY = 0x0,
+    READ_ONLY_ACCESSED = 0x1,
+    READ_WRITE = 0x10,
+    READ_WRITE_ACCESSED = 0x11,
+    READ_ONLY_EXPAND_DOWN = 0x100,
+    READ_ONLY_EXPAND_DOWN_ACCESSED = 0x101,
+    READ_WRITE_EXPAND_DOWN = 0x110,
+    READ_WRITE_EXPAND_DOWN_ACCESSED = 0x111
+};
+
+enum code_type_t {
+    EXECUTE_ONLY = 0x1000,
+    EXECUTE_ONLY_ACCESSED = 0x1001,
+    EXECUTE_READ = 0x1010,
+    EXECUTE_READ_ACCESSED = 0x1011,
+    EXECUTE_ONLY_CONFORMING = 0x1100,
+    EXECUTE_ONLY_CONFORMING_ACCESSED = 0x1101,
+    EXECUTE_READ_CONFORMING = 0x1110,
+    EXECUTE_READ_CONFORMING_ACCESSED = 0x1111
+};
+
+enum system_type_t {
+    TYPE_SYSTEM_RESERVED0 = 0x0000,
+    TYPE_BITS16_TSS_AVAILABLE = 0x0001,
+
+    TYPE_LDT = 0x0010,
+    TYPE_BITS16_TSS_BUSY = 0x0011,
+    TYPE_BITS16_CALL_GATE = 0x0100,
+    TYPE_TASK_GATE = 0x0101,
+    BITS16_INTERRUPT_GATE = 0x0110,
+    BITS16_TRAP_GATE = 0x0111,
+    TYPE_SYSTEM_RESERVED1 = 0x1000,
+    TYPE_BITS32_TSS_AVAILABLE = 0x1101,
+    TYPE_SYSTEM_RESERVED2 = 0x1010,
+    TYPE_BITS32_TSS_BUSY = 0x1011,
+    TYPE_BITS32_CALL_GATE = 0x1100,
+    TYPE_SYSTEM_RESERVED3 = 0x1101,
+    TYPE_BITS32_INTERRUPT = 0x1110,
+    TYPE_BITS32_TRAP_GATE = 0x1111
+};
+
+enum descriptor_type_t {
+    DESCRIPTOR_SYSTEM = 0,
+    DESCRIPTOR_CODE_OR_DATA = 1
+};
+
+enum default_op_size_t {
+    OP_SIZE_16 = 0,
+    OP_SIZE_32 = 1
+};
+
+enum granularity_t {
+    GRANULARITY_BYTE = 0,
+    GRANULARITY_PAGE = 1
+};
+
+#pragma pack(push, 1)
 struct segment_selector_t {
-
-    enum selector_table_t {
-        TABLE_GDT = 0,
-        TABLE_LDT = 1
-    };
-
     union {
         struct {
             uint16_t request_privilege_level : 2;
@@ -24,64 +81,6 @@ struct segment_selector_t {
 };
 
 struct segment_descriptor_t {
-
-    enum data_type_t {
-        READ_ONLY = 0x0,
-        READ_ONLY_ACCESSED = 0x1,
-        READ_WRITE = 0x10,
-        READ_WRITE_ACCESSED = 0x11,
-        READ_ONLY_EXPAND_DOWN = 0x100,
-        READ_ONLY_EXPAND_DOWN_ACCESSED = 0x101,
-        READ_WRITE_EXPAND_DOWN = 0x110,
-        READ_WRITE_EXPAND_DOWN_ACCESSED = 0x111
-    };
-
-    enum code_type_t {
-        EXECUTE_ONLY = 0x1000,
-        EXECUTE_ONLY_ACCESSED = 0x1001,
-        EXECUTE_READ = 0x1010,
-        EXECUTE_READ_ACCESSED = 0x1011,
-        EXECUTE_ONLY_CONFORMING = 0x1100,
-        EXECUTE_ONLY_CONFORMING_ACCESSED = 0x1101,
-        EXECUTE_READ_CONFORMING = 0x1110,
-        EXECUTE_READ_CONFORMING_ACCESSED = 0x1111
-    };
-
-    enum system_type_t {
-        TYPE_SYSTEM_RESERVED0 = 0x0000,
-        TYPE_BITS16_TSS_AVAILABLE = 0x0001,
-
-        TYPE_LDT = 0x0010,
-        TYPE_BITS16_TSS_BUSY = 0x0011,
-        TYPE_BITS16_CALL_GATE = 0x0100,
-        TYPE_TASK_GATE = 0x0101,
-        BITS16_INTERRUPT_GATE = 0x0110,
-        BITS16_TRAP_GATE = 0x0111,
-        TYPE_SYSTEM_RESERVED1 = 0x1000,
-        TYPE_BITS32_TSS_AVAILABLE = 0x1101,
-        TYPE_SYSTEM_RESERVED2 = 0x1010,
-        TYPE_BITS32_TSS_BUSY = 0x1011,
-        TYPE_BITS32_CALL_GATE = 0x1100,
-        TYPE_SYSTEM_RESERVED3 = 0x1101,
-        TYPE_BITS32_INTERRUPT = 0x1110,
-        TYPE_BITS32_TRAP_GATE = 0x1111
-    };
-
-    enum descriptor_type_t {
-        SYSTEM = 0,
-        CODE_OR_DATA = 1
-    };
-
-    enum default_op_size_t {
-        OP_SIZE_16 = 0,
-        OP_SIZE_32 = 1
-    };
-
-    enum granularity_t {
-        GRANULARITY_BYTE = 0,
-        GRANULARITY_PAGE = 1
-    };
-
     union {
         struct {
             uint64_t limit_low : 16;
@@ -97,30 +96,62 @@ struct segment_descriptor_t {
             uint64_t default_big : 1;
             uint64_t granularity : 1;
             uint64_t base_address_high : 8;
-            //uint64_t base_address_upper;
-            //uint64_t must_be_zero;
         } bits;
 
         uint64_t raw;
     };
 
-    void* base_address() const noexcept;
-    void base_address(void* value) noexcept;
+    virtual void* base_address() const noexcept;
+    virtual void base_address(void* value) noexcept;
 
-    uint32_t limit() const noexcept;
-    void limit(uint32_t value) noexcept;
+    virtual uint32_t limit() const noexcept;
+    virtual void limit(uint32_t value) noexcept;
 };
 
-struct segment_table_t {
+struct segment_descriptor64_t : segment_descriptor_t {
+    union {
+        struct {
+            uint64_t base_address_upper : 32;
+            uint64_t must_be_zero : 32;
+        } bits;
 
+        uint64_t raw;
+    } upper;
+
+    segment_descriptor64_t();
+
+    void* base_address() const noexcept override;
+    void base_address(void* value) noexcept override;
 };
 
-struct gdtr_t {
-    uint16_t limit;
+struct _segment_table_base_t {
+    virtual void* base_address() const noexcept = 0;
+    virtual uint16_t limit() const noexcept = 0;
+
+    void load() const noexcept;
+    void store() noexcept;
+
+    const segment_descriptor_t& operator[](const segment_selector_t& selector) const noexcept;
+    segment_descriptor_t& operator[](const segment_selector_t& selector) noexcept;
+    const segment_descriptor_t& operator[](int index) const noexcept;
+    segment_descriptor_t& operator[](int index) noexcept;
+};
+
+struct segment_table_t : _segment_table_base_t {
+    uint16_t lim;
     uint32_t base;
 
-    void load() noexcept;
-    void store() noexcept;
+    void* base_address() const noexcept override;
+    uint16_t limit() const noexcept override;
 };
+
+struct segment_table64_t : _segment_table_base_t {
+    uint16_t lim;
+    uint64_t base;
+
+    void* base_address() const noexcept override;
+    uint16_t limit() const noexcept override;
+};
+#pragma pack(pop)
 
 }
--- arch/src/x86/segment.cpp
+++ arch/src/x86/segment.cpp
@@ -27,10 +27,64 @@ void x86::segment_descriptor_t::limit(uint32_t value) noexcept {
     bits.limit_high = (value >> 16) & 0xf;
 }
 
-void x86::gdtr_t::load() noexcept {
-    _lgdt(this);
+x86::segment_descriptor64_t::segment_descriptor64_t() {
+    upper.bits.must_be_zero = 0;
 }
 
-void x86::gdtr_t::store() noexcept {
+void* x86::segment_descriptor64_t::base_address() const noexcept {
+    void* address_base = segment_descriptor_t::base_address();
+    uint64_t address = reinterpret_cast<uint64_t>(address_base);
+
+    if (descriptor_type_t::DESCRIPTOR_SYSTEM == bits.descriptor_type) {
+        address |= (static_cast<uint64_t>(upper.bits.base_address_upper) << 32);
+    }
+
+    return reinterpret_cast<void*>(address);
+}
+
+void x86::segment_descriptor64_t::base_address(void* value) noexcept {
+    segment_descriptor_t::base_address(value);
+
+    uint64_t address = reinterpret_cast<uint64_t>(value);
+    upper.bits.base_address_upper = (address >> 32) & 0xffff;
+}
+
+void x86::_segment_table_base_t::load() const noexcept {
+    _lgdt(const_cast<_segment_table_base_t*>(this));
+}
+
+void x86::_segment_table_base_t::store() noexcept {
     _sgdt(this);
+}
+
+const x86::segment_descriptor_t& x86::_segment_table_base_t::operator[](const segment_selector_t& selector) const noexcept {
+    return *reinterpret_cast<segment_descriptor_t*>(reinterpret_cast<uint64_t>(base_address()) + selector.bits.index * 8);
+}
+
+x86::segment_descriptor_t& x86::_segment_table_base_t::operator[](const segment_selector_t& selector) noexcept {
+    return *reinterpret_cast<segment_descriptor_t*>(reinterpret_cast<uint64_t>(base_address()) + selector.bits.index * 8);
+}
+
+const x86::segment_descriptor_t& x86::_segment_table_base_t::operator[](int index) const noexcept {
+    return reinterpret_cast<segment_descriptor_t*>(base_address())[index];
+}
+
+x86::segment_descriptor_t& x86::_segment_table_base_t::operator[](int index) noexcept {
+    return reinterpret_cast<segment_descriptor_t*>(base_address())[index];
+}
+
+void* x86::segment_table_t::base_address() const noexcept {
+    return reinterpret_cast<void*>(base);
+}
+
+uint16_t x86::segment_table_t::limit() const noexcept {
+    return lim;
+}
+
+void* x86::segment_table64_t::base_address() const noexcept {
+    return reinterpret_cast<void*>(base);
+}
+
+uint16_t x86::segment_table64_t::limit() const noexcept {
+    return lim;
 }

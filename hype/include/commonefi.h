cea2e54df2dbee3434bdf7da634b709fb3e384c4
Tom Tzook <tomtzook@gmail.com>
2021-01-16 14:24:54 +0200

    fixed cpp compilation

---
+++ hype/include/commonefi.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <efi.h>
+#include <efilib.h>
+
+#ifdef __cplusplus
+}
+#endif
--- hype/src/main.cpp
+++ hype/src/main.cpp
@@ -1,15 +1,18 @@
-extern "C" {
+#include "commonefi.h"
 
-#include <efi.h>
-#include <efilib.h>
-
-}
+#include <x86/cpuid.h>
 
 extern "C"
 EFI_STATUS EFIAPI
-efi_main (EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
+efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
 {
     InitializeLib(image_handle, system_table);
     Print((CHAR16*)L"Hello from the UEFI\n");
+
+    x86::cpuid_regs_t cpuid_regs = {};
+    x86::cpu_id(1, cpuid_regs);
+
+    Print((CHAR16*)L"CPUID[1].EAX=%x\n", cpuid_regs.eax);
+
     return EFI_SUCCESS;
 }

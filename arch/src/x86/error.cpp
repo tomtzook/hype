5c34daa4a061c703274fcb2ffd8787ecd41f0821
tomtzook <tomtzook@gmail.com>
2023-03-02 13:12:17 +0200

    gitignore

--- arch/src/x86/error.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "x86/error.h"
-
-
-template<>
-common::result::result_t(x86::result::code_t code) noexcept
-: m_code(code)
-, m_category(x86::result::CATEGORY)
-{}
--- arch/src/x86/intrinsic/igen.asm
+++
@@ -1,9 +0,0 @@
-
-bits 64
-
-section .text
-
-global _bit_scan_reverse
-_bit_scan_reverse:
-    bsr rax, rdi
-    ret
